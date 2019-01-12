06a72ac0207629612f6d3ac37f5c9b8d87c0a308
seductorAmadeus <seductorAmadeus@gmail.com>
2019-01-12 19:25:08 +0300

    Add some pages again

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,8 +1,63 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import * as ROUTES from '../../constants/routes'
+import { Link } from 'react-router-dom';
+
+const SignInWrapper = styled.div`
+  display: grid;
+  max-width: 300px;
+  position: absolute;
+  right: 50%;
+`;
+
+const Button = styled.button``;
+
+const Input = styled.input``;
 
 class SignIn extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      login: "",
+      password: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleClick(event) {
+    alert(this.state.password + " " + this.state.login);
+  }
+
   render() {
-    return <div>SignIn</div>;
+    return (
+      <SignInWrapper>
+        <Input
+          name="login"
+          value={this.state.login}
+          onChange={this.handleChange}
+          placeholder="Login: "
+        />
+        <Input
+          name="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+          placeholder="Password: "
+        />
+        <Button onClick={this.handleClick}>Войти</Button>
+        <Button>
+          <Link to={ROUTES.SIGNUP}>О нас</Link>
+        </Button>
+      </SignInWrapper>
+    );
   }
 }
 export default SignIn;
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,8 +1,63 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import * as ROUTES from "../../constants/routes";
+import { Link } from "react-router-dom";
+
+const SignUpWrapper = styled.div`
+  display: grid;
+  max-width: 300px;
+  position: absolute;
+  right: 50%;
+`;
+
+const Button = styled.button``;
+
+const Input = styled.input``;
 
 class SignUp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      login: "",
+      password: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleClick(event) {
+    alert(this.state.password + " " + this.state.login);
+  }
+
   render() {
-    return <div>SignUp</div>;
+    return (
+      <SignUpWrapper>
+        <Input
+          name="login"
+          value={this.state.login}
+          onChange={this.handleChange}
+          placeholder="Login: "
+        />
+        <Input
+          name="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+          placeholder="Password: "
+        />
+        <Button onClick={this.handleClick}>Зарегистироваться</Button>
+        <Button>
+          <Link to={ROUTES.SIGNIN}>Войти</Link>
+        </Button>
+      </SignUpWrapper>
+    );
   }
 }
 export default SignUp;
